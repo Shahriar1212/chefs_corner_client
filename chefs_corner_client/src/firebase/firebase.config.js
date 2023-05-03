bd1b4bcc2199e524db0374f23db2a5e0e0afd3d6
Shahriar1212 <shahriarmoin.aust@gmail.com>
2023-05-03 11:11:54 +0600

    project setup - added firebase

---
+++ chefs_corner_client/src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD17clTCRwHWwubwVBKRFK_qqq3e1nwl4g",
+  authDomain: "chefs-corner-9767f.firebaseapp.com",
+  projectId: "chefs-corner-9767f",
+  storageBucket: "chefs-corner-9767f.appspot.com",
+  messagingSenderId: "145481912250",
+  appId: "1:145481912250:web:6499e656136de85aba9783"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
