c015eddf82a44290b5c4daa92a7bc6aef08e962f
Shahriar1212 <shahriarmoin.aust@gmail.com>
2023-05-04 19:40:02 +0600

    some design fix

--- chefs_corner_client/src/components/RecipeCard/RecipeCard.jsx
+++ chefs_corner_client/src/components/RecipeCard/RecipeCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './RecipeCard.css'
+import { Link } from 'react-router-dom';
 const RecipeCard = (props) => {
 
-    const { recipe_name } = props.recipe;
+    const { recipe_name, img, id } = props.recipe;
     return (
         <div className='single-recipe-card'>
-            <img className='recipe-image img-fluid' src="../../../public/images/lawson.jpg" alt="" />
+            <img className='recipe-image img-fluid' src={img} alt="" />
             <h2 className='recipe-name'>{recipe_name}</h2>
-            <button className='btn btn-info'>View Details</button>
+            <Link to={`/chef/2/recipe/${id}`} className='btn btn-info'>View Details</Link>
         </div>
     );
 };
