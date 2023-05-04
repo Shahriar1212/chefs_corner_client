d7723f7e30d9c75c407a6f5dd6185f71e317aecc
Shahriar1212 <shahriarmoin.aust@gmail.com>
2023-05-04 10:49:08 +0600

    Footer added

--- chefs_corner_client/src/components/Footer/Footer.jsx
+++ chefs_corner_client/src/components/Footer/Footer.jsx
@@ -3,7 +3,37 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>footer</div>
+        <footer>
+            <div className='container'>
+                <div className="footer">
+                    
+                    <div className='col'>
+                        <h3>Chefs Corner</h3>
+                        <p>Experience culinary excellence with our expertly crafted dishes, made with the freshest ingredients. Taste the difference and book your table today!</p>
+
+                    </div>
+                    
+
+                    
+
+                    <div className='col'>
+                        <h4>Quick Links</h4>
+                        <a href="#">Help Desk</a>
+                        <a href="#">Sales</a>
+                        <a href="#">Developpers</a>
+
+                    </div>
+
+                    <div className='col'>
+                        <h4>Contact</h4>
+                        <p>524 Broadway</p>
+                        <p>+1 777 - 978 - 5570</p>
+                    </div>
+                </div>
+            </div>
+
+
+        </footer>
     );
 };
 
