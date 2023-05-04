8bb4df2916d8becc00cd4e255fb63641e404c35f
Shahriar1212 <shahriarmoin.aust@gmail.com>
2023-05-04 18:26:40 +0600

    chef info added

--- chefs_corner_client/src/components/ChefDetails/ChefDetails.jsx
+++ chefs_corner_client/src/components/ChefDetails/ChefDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import './ChefDetails.css'
 
 const ChefDetails = () => {
 
@@ -7,7 +8,29 @@ const ChefDetails = () => {
     console.log("loaded data is ", x);
     return (
         <div>
-            <h1>Chef Details</h1>
+            <section className='container chef-banner'>
+                <div className='chef-text'>
+                    <h2 className=''>Hi, I am {x.chef_name}.</h2>
+                </div>
+                
+                    <img className='chef-banner-image' src={x.img} alt="" />
+            </section>
+
+            <div className='container hr'></div>
+
+            <section className="container chef-info-section">
+                <div className="info-card">
+                    <p className='text-black'><span className='info-bold'>{x.number_of_likes} + </span> <br /> Total Likes</p>
+                </div>
+
+                <div className="info-card">
+                    <p className='text-black'><span className='info-bold'>{x.number_of_recipes} + </span> <br /> Number of Recipes</p>
+                </div>
+
+                <div className="info-card">
+                    <p className='text-black'><span className='info-bold'>{x.years_of_experience} + </span> <br /> Total years of experience</p>
+                </div>
+            </section>
         </div>
     );
 };
