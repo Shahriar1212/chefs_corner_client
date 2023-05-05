27fb30d3c9def1e6e31fbc94b7f7f630fd500cf2
Shahriar1212 <shahriarmoin.aust@gmail.com>
2023-05-05 23:27:17 +0600

    env variable handled

--- chefs_corner_client/src/firebase/firebase.config.js
+++ chefs_corner_client/src/firebase/firebase.config.js
@@ -4,14 +4,18 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD17clTCRwHWwubwVBKRFK_qqq3e1nwl4g",
-  authDomain: "chefs-corner-9767f.firebaseapp.com",
-  projectId: "chefs-corner-9767f",
-  storageBucket: "chefs-corner-9767f.appspot.com",
-  messagingSenderId: "145481912250",
-  appId: "1:145481912250:web:6499e656136de85aba9783"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
